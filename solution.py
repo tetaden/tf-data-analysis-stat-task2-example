c4ea3af42132a790faccbf7e9aa1ecad81f1e41e
tetaden <84914528+tetaden@users.noreply.github.com>
2023-04-05 19:09:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, chi
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 415542660 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
@@ -12,6 +12,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    scale = ( x * x ).sum() / n
+    return np.sqrt(scale * (n - 1) / chi.ppf(alpha, n - 1)), \
+           np.sqrt(scale * (n - 1) / chi.ppf(alpha /2, n - 1))
